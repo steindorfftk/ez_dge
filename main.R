4ec2e8b037f4feeaaddee0a2db5ede0288a4d4bd
steindorfftk <147119154+steindorfftk@users.noreply.github.com>
2023-10-20 13:12:14 -0300

    Add files via upload

--- main.R
+++ main.R
@@ -7,21 +7,30 @@ setwd("~/Desktop/Coding/deaR")
 #BiocManager::install("edgeR")
 library("edgeR")
 # BiocManager::install("sva")
-library("sva")
+#library("sva")
 # BiocManager::install("org.Hs.eg.db")
-library("org.Hs.eg.db")
+#library("org.Hs.eg.db")
 
-#Select files
+#01 - readDGE
 cond1_dir <- "input/cond1" #Set condition 1 directory path
 cond2_dir <- "input/cond2" #Set condition 2 directory path
 cond1_files <- list.files(cond1_dir, pattern = "\\.tabular$", full.names = TRUE) #Save cond1 files
 cond2_files <- list.files(cond2_dir, pattern = "\\.tabular$", full.names = TRUE) #Save cond2 files
 all_files <- c(cond1_files, cond2_files) #Combine sets of files
-DG <- readDGE(all_files, header=T)
+DG <- readDGE(all_files, header=T) #Run readDGE
 
+#02 - Load files
+# Load files for cond1
+cond1_files <- list.files('input/cond1', pattern = "\\.tabular$", full.names = TRUE)
+for (i in seq_along(cond1_files)) {
+  assign(paste0("cond_1_", i), read.table(cond1_files[i], sep = '\t', header = TRUE))
+}
 
-
-
+# Load files for cond2
+cond2_files <- list.files('input/cond2', pattern = "\\.tabular$", full.names = TRUE)
+for (i in seq_along(cond2_files)) {
+  assign(paste0("cond_2_", i), read.table(cond2_files[i], sep = '\t', header = TRUE))
+}
 
 
 
